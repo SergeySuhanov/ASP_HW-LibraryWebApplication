b25dc783a1dc444a2369dcce8c4f72a990810666
SergeySuhanov <beent@yandex.ru>
2023-11-19 23:40:45 +0300

    Phase 5 - Editing, Genres smart adding-removing

--- LibraryWebDb/Controllers/LibraryController.cs
+++ LibraryWebDb/Controllers/LibraryController.cs
@@ -1,4 +1,5 @@
-﻿using LibraryWebDb.Helpers;
+﻿using LibraryWebDb.Extensions;
+using LibraryWebDb.Helpers;
 using LibraryWebDb.Models;
 using LibraryWebDb.ViewModels;
 using Microsoft.AspNetCore.Mvc;
@@ -19,7 +20,7 @@ namespace LibraryWebDb.Controllers
 		[HttpGet]
 		public IActionResult Index()
 		{
-			var books = libraryDbContext.Books;
+			var books = libraryDbContext.Books.Include(b => b.BookGenres).ThenInclude(bg => bg.Genre).Include(b => b.Category);
 			var categories = libraryDbContext.Categories;
 			var genres = libraryDbContext.Genres;
 
@@ -40,8 +41,7 @@ namespace LibraryWebDb.Controllers
 		[ValidateAntiForgeryToken]
 		public async Task<IActionResult> Add(Book book, IFormFile Image, int[] genres)
 	 	{
-			//if (ModelState.IsValid)
-			//{
+			
 			book.ImageUrl = await FileUploadHelper.UploadAsync(Image);
 			book.CreatedDate = DateTime.Now;
 			await libraryDbContext.Books.AddAsync(book);
@@ -51,21 +51,56 @@ namespace LibraryWebDb.Controllers
             await libraryDbContext.SaveChangesAsync();
 
             return RedirectToAction("Index");
-			//}
+		}
+
+		[HttpGet]
+		public IActionResult Details(int bookId)
+		{
+			var book = libraryDbContext.Books
+				.Include(b => b.BookGenres).ThenInclude(bg => bg.Genre)
+				.Include(b => b.Category)
+				.FirstOrDefault(b => b.Id == bookId);
 			return View(book);
 		}
 
 		[HttpGet]
 		public IActionResult Edit(int bookId)
 		{
-			var book = libraryDbContext.Books.Include(b => b.Category).Include(b => b.BookGenres).FirstOrDefault(b => b.Id == bookId);
+			var book = libraryDbContext.Books.Find(bookId);
+
+			ViewBag.categories = new SelectList(libraryDbContext.Categories, "Id", "Name");
+
+			var selectedGenresIds = libraryDbContext.BookGenres.Where(bg => bg.BookId == bookId).Select(g => g.GenreId);
+			ViewBag.genres = new MultiSelectList(libraryDbContext.Genres, "Id", "Name", selectedGenresIds);
 			return View(book);
 		}
 
-		[HttpPost]
-		public async Task<IActionResult> Edit(Book book, IFormFile Image, int[] genres)
-		{
-			return RedirectToAction("Index");
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Book book, IFormFile Image, int[] genres)
+        {
+			if (Image != null)
+			{
+				var path = await FileUploadHelper.UploadAsync(Image);
+				book.ImageUrl = path;
+			}
+			
+			book.CreatedDate = DateTime.Now;
+			libraryDbContext.Books.Update(book);
+			await libraryDbContext.SaveChangesAsync();
+
+			var bookWithGenres = libraryDbContext.Books.Include(b => b.BookGenres).FirstOrDefault(b => b.Id == book.Id);
+
+			libraryDbContext.UpdateManyToMany(
+				bookWithGenres.BookGenres,
+				genres.Select(gId => new BookGenre { BookId = book.Id, GenreId = gId}),
+				gId => gId.GenreId
+				);
+			await libraryDbContext.SaveChangesAsync();
+
+			///----- Stopped here at 2:23:44
+
+            return RedirectToAction("Index");
 		}
 	}
 }
---
+++ LibraryWebDb/Extensions/DbContextExtensions.cs
@@ -0,0 +1,28 @@
+﻿using LibraryWebDb.Models;
+
+namespace LibraryWebDb.Extensions
+{
+    public static class DbContextExtensions
+    {
+        public static void UpdateManyToMany<T,KEY>(this LibraryDbContext libraryDbContext, IEnumerable<T>currentItems, IEnumerable<T> newItems, Func<T,KEY> getKey) where T : class
+        {
+            if (currentItems == null)
+            {
+                libraryDbContext.Set<T>().RemoveRange(currentItems.Except(newItems, getKey));
+                libraryDbContext.Set<T>().AddRange(newItems.Except(currentItems, getKey));
+            }
+            else
+            {
+                libraryDbContext.Set<T>().AddRange(newItems);
+            }
+        }
+
+        public static IEnumerable<T> Except<T,KEY>(this IEnumerable<T> items, IEnumerable<T> other, Func<T,KEY> getKeyFunc)
+        {
+            return items.GroupJoin(items, getKeyFunc, getKeyFunc, (item, templateItems) => new { item, templateItems })
+                .SelectMany(t => t.templateItems.DefaultIfEmpty(), (t, tmp) => new { t, tmp })
+                .Where(t => ReferenceEquals(null, t.tmp) || t.tmp.Equals(default(T)))
+                .Select(t => t.t.item);
+        }
+    }
+}
